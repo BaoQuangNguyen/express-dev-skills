c03c66e7caccdce3d2110837c9ddeb9a63f32a15
BaoQuangNguyen <bnguyen0239@gmail.com>
2023-06-14 20:04:33 -0500

    express dev skills lab

---
+++ controllers/skills.js
@@ -0,0 +1,34 @@
+module.exports = {
+  index,
+  show,
+  new: newSkill,
+  create,
+  delete: deleteSkill
+}
+
+const Skill = require('../models/skill');
+
+
+function index(req, res) {
+  const skills = Skill.getAll();
+  res.render('skills/index', {skills})
+}
+
+function show(req, res) {
+  const skill = Skill.getOne(req.params.id);
+  res.render('skills/show', {skill})
+}
+
+function newSkill(req, res) {
+  res.render('skills/new', {title: 'New Skill'})
+}
+
+function create(req, res) {
+  Skill.create(req.body);
+  res.redirect('/skills');
+}
+
+function deleteSkill(req, res) {
+  Skill.deleteOne(req.params.id);
+  res.redirect('/skills');
+}
---
+++ models/skill.js
@@ -0,0 +1,36 @@
+module.exports = {
+  getAll,
+  getOne,
+  create,
+  deleteOne
+}
+const skills = [
+    {id: 238492, language: 'HTML', os: 'Mac OS Ventura', tools: 'Visual Studio Code'},
+    {id: 243533, language: 'CSS', os: 'Mac OS Ventura', tools: 'Visual Studio Code'},
+    {id: 253453, language: 'JavaScript', os: 'Mac OS Ventura', tools: 'Visual Studio Code'},
+    
+  ]
+	
+  function getAll() {
+    return skills;
+  }
+  
+  function getOne(id) {
+    id = parseInt(id);
+    return skills.find(function(skill) {
+      return skill.id === id;
+    })
+  }
+  
+  function create(skill) {
+    skill.id = Date.now() % 1000000;
+    skill.os = 'Mac OS Ventura';
+    skill.tools = 'Visual Studio Code';
+    skills.push(skill);
+  }
+
+  function deleteOne(id) {
+    id = parseInt(id);
+    const idx = skills.findIndex(skill => skill.id === id);
+    skills.splice(idx, 1);
+  }
